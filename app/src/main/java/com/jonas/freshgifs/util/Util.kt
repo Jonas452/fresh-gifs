51b239a29ebd773f0e0b6b4cb9f9b956c9ee3702
Jonas <jonasjordao452@gmail.com>
2022-05-03 14:26:37 -0300

    configure api key

---
+++ app/src/main/java/com/jonas/freshgifs/util/Util.kt
@@ -0,0 +1,8 @@
+package com.jonas.freshgifs.util
+
+import com.jonas.freshgifs.BuildConfig
+
+object Util  {
+    const val BASE_URL = "https://api.giphy.com/v1/"
+    const val API_KEY = BuildConfig.API_KEY
+}
