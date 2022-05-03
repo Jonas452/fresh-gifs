3b91ddc0ac6a8184eadef1f9871d100bd8ab6e2a
Jonas <jonasjordao452@gmail.com>
2022-05-03 07:29:26 -0300

    make repo thread safe

--- app/src/main/java/com/jonas/freshgifs/data/repository/FreshGIFSRepositoryImpl.kt
+++ app/src/main/java/com/jonas/freshgifs/data/repository/FreshGIFSRepositoryImpl.kt
@@ -2,23 +2,29 @@ package com.jonas.freshgifs.data.repository
 
 import com.jonas.freshgifs.data.remote.FreshGIFSRemoteDataSource
 import com.jonas.freshgifs.data.remote.mapper.GIFRemoteMapper
+import com.jonas.freshgifs.di.IOContext
 import com.jonas.freshgifs.domain.model.GIF
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 class FreshGIFSRepositoryImpl @Inject constructor(
     private val freshGIFSRemoteDataSource: FreshGIFSRemoteDataSource,
     private val gifRemoteMapper: GIFRemoteMapper,
+    @IOContext private val coroutineContext: CoroutineContext,
 ): FreshGIFSRepository {
 
-    override suspend fun getTrendingGIFS(apiKey: String): List<GIF> {
-        return freshGIFSRemoteDataSource.getTrendingGIFS(apiKey).data.map {
-            gifRemoteMapper.fromGIFResponse(it)
+    override suspend fun getTrendingGIFS(apiKey: String): List<GIF> =
+        withContext(coroutineContext) {
+            return@withContext freshGIFSRemoteDataSource.getTrendingGIFS(apiKey).data.map {
+                gifRemoteMapper.fromGIFResponse(it)
+            }
         }
-    }
 
-    override suspend fun searchGIFS(apiKey: String, query: String): List<GIF> {
-        return freshGIFSRemoteDataSource.searchGIFS(apiKey, query).data.map {
-            gifRemoteMapper.fromGIFResponse(it)
+    override suspend fun searchGIFS(apiKey: String, query: String): List<GIF> =
+        withContext(coroutineContext) {
+            return@withContext freshGIFSRemoteDataSource.searchGIFS(apiKey, query).data.map {
+                gifRemoteMapper.fromGIFResponse(it)
+            }
         }
-    }
 }
