20877ff13d2751a6d5ac88bd71237ed6f429f22e
Jonas <jonasjordao452@gmail.com>
2022-05-11 09:30:53 -0300

    add tests for the state of the view model

--- app/src/test/java/com/jonas/freshgifs/ui/discover/DiscoverViewModelTest.kt
+++ app/src/test/java/com/jonas/freshgifs/ui/discover/DiscoverViewModelTest.kt
@@ -1,14 +1,11 @@
 package com.jonas.freshgifs.ui.discover
 
-import com.google.common.truth.Truth
 import com.google.common.truth.Truth.assertThat
 import com.jonas.freshgifs.domain.usecase.*
 import com.jonas.freshgifs.util.createGIF
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.last
-import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.*
 import org.junit.Before
@@ -62,6 +59,47 @@ class DiscoverViewModelTest {
 
         // Assert
         assertThat(results[1]).isInstanceOf(DiscoverUIState.Loading::class.java)
+        job.cancel()
+    }
+
+    @Test
+    fun `when getting trending gifs should return success as state`() = runTest {
+        // Arrange
+        val gifsList = listOf(
+            createGIF(),
+            createGIF(),
+            createGIF(),
+        )
+        coEvery { getTrendingGIFSUseCase() } coAnswers  {
+            gifsList
+        }
+
+        // Act
+        val results = mutableListOf<DiscoverUIState>()
+        val job = launch(testDispatcher) {
+            subject.discoverUIState.toList(results)
+        }
+        subject.getTrendingGIFS()
+
+        // Assert
+        assertThat(results.last()).isInstanceOf(DiscoverUIState.Success::class.java)
+        job.cancel()
+    }
+
+    @Test
+    fun `when getting trending gifs error should return error as state`() = runTest {
+        // Arrange
+        coEvery { getTrendingGIFSUseCase() } throws Exception("Test Exception")
+
+        // Act
+        val results = mutableListOf<DiscoverUIState>()
+        val job = launch(testDispatcher) {
+            subject.discoverUIState.toList(results)
+        }
+        subject.getTrendingGIFS()
+
+        // Assert
+        assertThat(results.last()).isInstanceOf(DiscoverUIState.Error::class.java)
         job.cancel()
     }
 }
